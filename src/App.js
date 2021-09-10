6d17615eeba9276929d4fe6bd611c7d216f282d1
KuzMich1994 <drumer19940310@mail.ru>
2021-09-10 22:48:27 +0300

    create: added PokemonCard component and added function of turn card (handlerClick)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Header from "./components/Header/Header.jsx";
 import Layout from "./components/Layout/Layout.jsx";
 import bgUrl from './components/Layout/img/background.jpg';
 import Footer from "./components/Footer/Footer.jsx";
+import './App.css';
+import POKEMON_LIST from './api/pokemon-list.json';
+import PokemonCard from "./components/PokemonCard/PokemonCard.jsx";
 
 function App() {
   const title = 'This is title';
@@ -9,9 +12,32 @@ function App() {
   return (
     <>
       <Header title={title} descr={descr} />
-      <Layout title={title} descr={descr} bgUrl={bgUrl} />
-      <Layout title={title} descr={descr} colorBg={'#e2e2e2'} />
-      <Layout title={title} descr={descr} bgUrl={bgUrl} />
+      <Layout 
+        title={title}
+        bgUrl={bgUrl}
+      >
+        <p>In the game two players face off against one another, one side playing as "blue", the other as "red" on a 3x3 grid.</p>
+      </Layout>
+      <Layout 
+        title={title}
+        colorBg={'#e2e2e2'}
+      >
+        <div className='flex'>
+          {
+            POKEMON_LIST.map(item => <PokemonCard 
+              img={item.img}
+              key={item.id}
+              values={item.values}
+              name={item.name}
+              type={item.type}
+              id={item.id}
+            />)
+          }
+        </div>
+      </Layout>
+      <Layout title={title} bgUrl={bgUrl}>
+        <p>To win, a majority of the total ten cards played (including the one card that is not placed on the board) must be of the player's card color. To do this, the player must capture cards by placing a card adjacent to an opponent's card whereupon the 'ranks' of the sides where the two cards touch will be compared. If the rank of the opponent's card is higher than the player's card, the player's card will be captured and turned into the opponent's color. If the player's rank is higher, the opponent's card will be captured and changed into the player's color instead.</p>
+      </Layout>
       <Footer />
     </>
   );
